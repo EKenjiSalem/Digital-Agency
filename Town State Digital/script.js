164e6e7daa66fcea042d218d49369b70e8cef4b5
Elias K. Salem <EliasKSalemTech@gmail.com>
2022-10-23 17:54:47 -0700

    Update script.js

--- Town State Digital/script.js
+++ Town State Digital/script.js
@@ -10,9 +10,6 @@ menu.onclick = () => {
 
 // Scroll Bar 
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
-     navbar.classList.remove('active');
-
     if (window.scrollY > 0)  {
         header.classList.add('active');
     } else {
